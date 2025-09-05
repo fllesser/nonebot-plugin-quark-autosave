e9a38ade97b3556f47816d66a46356a70edf00bf
Les Freire <64878354+fllesser@users.noreply.github.com>
2025-09-05 12:30:52 +0800

    ✨ feat: 基本实现 (#1)
    ...

--- src/nonebot_plugin_quark_autosave/config.py
+++ src/nonebot_plugin_quark_autosave/config.py
@@ -3,7 +3,9 @@ from pydantic import BaseModel
 
 
 class Config(BaseModel):
-    pass
+    quark_autosave_endpoint: str = "http://127.0.0.1:5005"
+    quark_autosave_token: str | None = None
+    quark_auto_save_path_base: str = "夸克自动转存"
 
 
 # 配置加载
---
+++ tests/test_auto_save.py
@@ -0,0 +1,37 @@
+# import json
+
+# from nonebot import logger
+
+
+# async def test_get_data():
+#     from nonebot_plugin_quark_autosave.client import QASClient
+
+#     async with AutoSaveCl   ient() as client:
+#         data = await client.get_data()
+#     logger.info(data)
+
+
+# async def test_get_share_detail():
+#     from nonebot_plugin_quark_autosave.client import QASClient
+
+#     async with AutoSaveClient() as client:
+#         await client.create_task("基地第三季哈哈哈", "https://pan.quark.cn/s/e06704643151")
+
+
+# async def test_update():
+#     from nonebot_plugin_quark_autosave.client import AutoSaveClient
+#     from nonebot_plugin_quark_autosave.config import data_dir
+
+#     async with AutoSaveClient() as client:
+#         with open(
+#             data_dir / "update_playload.json",
+#         ) as f:
+#             data = json.load(f)
+#             await client.client.post("/update", json=data)
+
+
+# async def test_add_task():
+#     from nonebot_plugin_quark_autosave.client import QASClient
+
+#     async with QASClient() as client:
+#         await client.add_task("基地第三季测试", "https://pan.quark.cn/s/e06704643151")
