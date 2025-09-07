6c2d71d861ea3a9e07fe30d33e9d349d5637e011
Les Freire <64878354+fllesser@users.noreply.github.com>
2025-09-07 09:56:42 +0800

    🐳 chore: use poethepoet to test (#6)
    * 🐳 chore: use poethepoet to test
    
    * 🐳 chore: 修改 code cov

--- tests/plugin_test.py
+++ tests/plugin_test.py
@@ -1,34 +1,5 @@
-from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, Message  # noqa: F401
 from nonebug import App
 import pytest
-
-
-def make_onebot_event(message: Message) -> GroupMessageEvent:
-    from random import randint
-    from time import time
-
-    from nonebot.adapters.onebot.v11.event import Sender
-
-    message_id = randint(1000000000, 9999999999)
-    user_id = randint(1000000000, 9999999999)
-    group_id = 123456789
-
-    event = GroupMessageEvent(
-        time=int(time()),
-        sub_type="normal",
-        self_id=123456,
-        post_type="message",
-        message_type="group",
-        message_id=message_id,
-        user_id=user_id,
-        group_id=group_id,
-        raw_message=message.extract_plain_text(),
-        message=message,
-        original_message=message,
-        sender=Sender(user_id=user_id, nickname="TestUser"),
-        font=123456,
-    )
-    return event
 
 
 @pytest.mark.asyncio
