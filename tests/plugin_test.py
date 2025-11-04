89d7f2bd496d9c6b6ea2aa44ba0a3bd49649947b
fllesser <fllessive@gmail.com>
2025-11-04 11:29:20 +0800

    Update SHARE_URL_REGEX to properly escape dots and add test cases for URLs with password parameters

--- tests/plugin_test.py
+++ tests/plugin_test.py
@@ -15,6 +15,8 @@ def test_share_url_regex():
     url_list = [
         "https://pan.quark.cn/s/e06704643151",
         "https://pan.quark.cn/s/e06704643151#/list/share/4afa4cd5bf0e4e7bb1a2ccef8f094d74",
+        "https://pan.quark.cn/s/e06704643151?pwd=12e4L6",
+        "https://pan.quark.cn/s/203e7e764160?pwd=FLtm#/list/share/aabc9f149afc425bb8a81a51402bb5a1",
     ]
 
     for url in url_list:
